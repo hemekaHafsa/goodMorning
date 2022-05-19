6989d04f5f659ea24cb36e942a43835c5f459c7b
Hafsa <hemekahafsa@gmail.com>
2022-05-19 07:36:47 +0600

    updated

--- Untitled6.c
+++ Untitled6.c
@@ -4,7 +4,7 @@ int main()
 {
   int n,i,j;
   printf("Multiplication table for number 1 to 10");
-
+//this is a for loop
   for(i=1;i<=10;i++){
     for(j=1;j<=10;j++){
         printf("%d*%d=%d\n",i,j,i*j);
--- test.cpp
+++ test.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main(){
 cout<<"good morning";
 cout<<"good night";
+cout<<"Hafsa";
+cout<<"Hello!";
 return 0;
 }
